688fd9db71d3ea9033343d5e392fea97e3749e0c
月之领主LM <178910432@qq.com>
2020-03-26 00:07:55 +0800

    Update

--- src/cn/moonlord/Main.java
+++ src/cn/moonlord/Main.java
@@ -1,5 +1,7 @@
 package cn.moonlord;
 
+import cn.moonlord.task.TaskManager;
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
@@ -9,6 +11,7 @@ public class Main {
         //HttpStressTest.test2();
         //M3U8VideoDownloaderTest.test1();
 
+        new TaskManager("out/task.config").runDailyCycle();
     }
 
 }
