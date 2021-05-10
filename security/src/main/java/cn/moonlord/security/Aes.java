593568864c31a11591bc27259dc34a22e794977b
月之领主LM <178910432@qq.com>
2021-05-11 02:08:29 +0800

    Update

--- security/src/main/java/cn/moonlord/security/Aes.java
+++ security/src/main/java/cn/moonlord/security/Aes.java
@@ -27,8 +27,17 @@ public class Aes {
         return Base64.encode(generateKey());
     }
 
-    public static SecretKeySpec getSecretKey(byte[] key) {
-        return new SecretKeySpec(key, AES_KEY_ALGORITHM);
+    public static SecretKeySpec getSecretKey(byte[] keyBytes) {
+        if(keyBytes == null){
+            throw new IllegalArgumentException("Aes getSecretKey error, keyBytes must not be null");
+        }
+        if(keyBytes.length == 0){
+            throw new IllegalArgumentException("Aes getSecretKey error, keyBytes must not be empty");
+        }
+        if(keyBytes.length != AES_KEY_LENGTH  / Byte.SIZE){
+            throw new IllegalArgumentException("Aes getSecretKey error, the length of keyBytes [" + keyBytes.length + "] must be " + ( AES_KEY_LENGTH  / Byte.SIZE ));
+        }
+        return new SecretKeySpec(keyBytes, AES_KEY_ALGORITHM);
     }
 
     public static SecretKeySpec getSecretKey(String keyBase64String) {
@@ -36,8 +45,11 @@ public class Aes {
     }
 
     public static byte[] encrypt(byte[] sourceBytes, SecretKeySpec encryptKey) throws Exception {
+        if(sourceBytes == null){
+            throw new IllegalArgumentException("Aes encrypt error, sourceBytes must not be null");
+        }
         if(encryptKey.getEncoded().length != AES_KEY_LENGTH  / Byte.SIZE){
-            throw new InvalidAlgorithmParameterException("encrypt key length is not match, the length should be " + AES_KEY_LENGTH);
+            throw new InvalidAlgorithmParameterException("Aes encrypt error, encryptKey length must be " + AES_KEY_LENGTH);
         }
 
         byte[] iv = Random.generate(GCM_IV_LENGTH);
@@ -77,12 +89,18 @@ public class Aes {
     }
 
     public static byte[] decrypt(byte[] encryptedBytes, SecretKeySpec decryptKey) throws Exception {
+        if(encryptedBytes == null){
+            throw new IllegalArgumentException("Aes decrypt error, encryptedBytes must not be null");
+        }
+        if(decryptKey.getEncoded().length != AES_KEY_LENGTH  / Byte.SIZE){
+            throw new InvalidAlgorithmParameterException("Aes decrypt error, decryptKey length must be " + AES_KEY_LENGTH);
+        }
+
         byte[] iv = Arrays.copyOfRange(encryptedBytes, 0, GCM_IV_LENGTH / Byte.SIZE);
         Cipher cipher = Cipher.getInstance(AES_CIPHER_INSTANCE);
         GCMParameterSpec gcmSpec = new GCMParameterSpec(GCM_TAG_LENGTH, iv);
         cipher.init(Cipher.DECRYPT_MODE, decryptKey, gcmSpec);
-        byte[] decryptedSource = cipher.doFinal(encryptedBytes, iv.length, encryptedBytes.length - iv.length);
-        return decryptedSource;
+        return cipher.doFinal(encryptedBytes, iv.length, encryptedBytes.length - iv.length);
     }
 
     public static byte[] decrypt(byte[] encryptedBytes, byte[] decryptKeyBytes) throws Exception {
@@ -94,15 +112,18 @@ public class Aes {
     }
 
     public static String decryptString(byte[] encryptedBytes, SecretKeySpec decryptKey) throws Exception {
-        return new String(decrypt(encryptedBytes, decryptKey), StandardCharsets.UTF_8);
+        byte[] sourceBytes = decrypt(encryptedBytes, decryptKey);
+        return new String(sourceBytes, StandardCharsets.UTF_8);
     }
 
     public static String decryptString(byte[] encryptedBytes, byte[] decryptKeyBytes) throws Exception {
-        return new String(decrypt(encryptedBytes, decryptKeyBytes), StandardCharsets.UTF_8);
+        byte[] sourceBytes = decrypt(encryptedBytes, decryptKeyBytes);
+        return new String(sourceBytes, StandardCharsets.UTF_8);
     }
 
     public static String decryptString(byte[] encryptedBytes, String decryptKeyBase64String) throws Exception {
-        return new String(decrypt(encryptedBytes, decryptKeyBase64String), StandardCharsets.UTF_8);
+        byte[] sourceBytes = decrypt(encryptedBytes, decryptKeyBase64String);
+        return new String(sourceBytes, StandardCharsets.UTF_8);
     }
 
 }
