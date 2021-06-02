76f9789f8ba2ef1c640d2eece387db31e68ff190
月之领主LM <178910432@qq.com>
2021-06-03 03:04:49 +0800

    Update

--- security/src/test/java/cn/moonlord/security/AesTest.java
+++ security/src/test/java/cn/moonlord/security/AesTest.java
@@ -129,10 +129,18 @@ public class AesTest {
     public static class decrypt {
         @Test
         public void success_1() {
+            String source = "AAAAAAAAAAAAAAAAUw+K+8dFNrmpY7TxxMtziw==";
+            String key = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+            byte[] result = Aes.decrypt(Base64.decode(source), key);
+            Assert.assertEquals("success_1", 0, result.length);
+        }
+
+        @Test
+        public void success_2() {
             String source = "Bq3mORrbjFvklrGN0p8tk56t/Q8nPxrwWvccsg==";
             String key = "FA2AWG9YgCE8k+ypI+EDuUcJEi4H3bp3BQbQHkc0tJQ=";
             byte[] result = Aes.decrypt(Base64.decode(source), key);
-            Assert.assertEquals("success_1", 0, result.length);
+            Assert.assertEquals("success_2", 0, result.length);
         }
 
         @Test(expected = IllegalArgumentException.class)
