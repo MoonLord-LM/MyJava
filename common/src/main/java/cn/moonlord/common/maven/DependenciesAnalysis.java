c79a6b1005929564ef4d114bdafe4fccf8735b9d
MoonLord-LM <178910432@qq.com>
2021-11-03 15:57:33 +0800

    Update

--- common/src/main/java/cn/moonlord/common/maven/DependenciesAnalysis.java
+++ common/src/main/java/cn/moonlord/common/maven/DependenciesAnalysis.java
@@ -2,16 +2,19 @@ package cn.moonlord.common.maven;
 
 import cn.moonlord.common.ssl.TrustAllCerts;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.Serializable;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Objects;
+import java.util.Map;
 
 public class DependenciesAnalysis implements Runnable {
 
@@ -19,11 +22,11 @@ public class DependenciesAnalysis implements Runnable {
 
     private final String outputDependencyFilePath;
 
-    public DependenciesAnalysis(String imputDependencyManagementFilePath, String outputDependencyFilePath) {
-        this.inputDependencyManagementFilePath = imputDependencyManagementFilePath;
+    public DependenciesAnalysis(String inputDependencyManagementFilePath, String outputDependencyFilePath) {
+        this.inputDependencyManagementFilePath = inputDependencyManagementFilePath;
         this.outputDependencyFilePath = outputDependencyFilePath;
-        if(!Files.exists(Paths.get(imputDependencyManagementFilePath))){
-            throw new IllegalArgumentException("imputDependencyManagementFilePath must be a valid File");
+        if(!Files.exists(Paths.get(inputDependencyManagementFilePath))){
+            throw new IllegalArgumentException("inputDependencyManagementFilePath must be a valid File");
         }
         if(!Files.exists(Paths.get(outputDependencyFilePath))){
             throw new IllegalArgumentException("outputDependencyFilePath must be a valid File");
@@ -32,79 +35,248 @@ public class DependenciesAnalysis implements Runnable {
         System.out.println("this.outputDependencyFilePath: " + this.outputDependencyFilePath);
     }
 
-    private String getTagString(String inputLine, String TagString, String currentValue) {
-        String tmp = currentValue;
-        if(tmp == null && inputLine.startsWith("<" + TagString + ">")) {
+    public static class Dependency implements Serializable {
+
+        public static final String BASE_RELEASE_URL = "https://mvnrepository.com/artifact/";
+
+        public static final String BASE_DOWNLOAD_URL = "https://repo1.maven.org/maven2/";
+
+        private String releaseUrl;
+
+        private String downloadUrl;
+
+        private String fileName;
+
+        private Map<String, String> properties;
+
+        private String groupId;
+
+        private String artifactId;
+
+        private String version;
+
+        private String classifier;
+
+        private String type;
+
+        private String scope;
+
+        @Override
+        public String toString(){
+            return "" +
+                    "        <dependency>" + "\r\n" +
+                    "            <groupId>" + groupId + "</groupId>" + "\r\n" +
+                    "            <artifactId>" + artifactId + "</artifactId>" + "\r\n" +
+                    ( classifier == null ? "" : (
+                    "            <classifier>" + classifier + "</classifier>" + "\r\n"
+                    )) +
+                    ( type == null ? "" : (
+                    "            <type>" + type + "</type>" + "\r\n"
+                    )) +
+                    "        </dependency>" + "\r\n";
+        }
+
+        public String getReleaseUrl() {
+            return releaseUrl;
+        }
+
+        public void setReleaseUrl(String releaseUrl) {
+            this.releaseUrl = releaseUrl;
+        }
+
+        public String getDownloadUrl() {
+            if(downloadUrl == null && releaseUrl != null && getFileName() != null) {
+                return releaseUrl.replace(".", "/").replace(BASE_RELEASE_URL.replace(".", "/"), BASE_DOWNLOAD_URL) + "/" + version + "/" + getFileName();
+            }
+            if(downloadUrl == null && groupId != null && getFileName() != null) {
+                return BASE_RELEASE_URL + groupId.replace(".", "/") + "/" + version + "/" + getFileName();
+            }
+            return downloadUrl;
+        }
+
+        public void setDownloadUrl(String downloadUrl) {
+            this.downloadUrl = downloadUrl;
+        }
+
+        public String getFileName() {
+            if(fileName == null  && artifactId != null && version != null) {
+                return artifactId  + "-" + version + ( classifier == null ? "" : "-" + classifier) + "." + ( type == null ? "jar" : type);
+            }
+            return fileName;
+        }
+
+        public void setFileName(String fileName) {
+            this.fileName = fileName;
+        }
+
+        public Map<String, String> getProperties() {
+            if(properties == null) {
+                return new HashMap<>();
+            }
+            return properties;
+        }
+
+        public void setProperties(Map<String, String> properties) {
+            this.properties = properties;
+        }
+
+        public String getGroupId() {
+            return groupId;
+        }
+
+        public void setGroupId(String groupId) {
+            this.groupId = groupId;
+        }
+
+        public String getArtifactId() {
+            return artifactId;
+        }
+
+        public void setArtifactId(String artifactId) {
+            this.artifactId = artifactId;
+        }
+
+        public String getVersion() {
+            if(version != null && version.startsWith("${") && version.endsWith("}")) {
+                String key = StringUtils.substringBetween(version, "${", "}");
+                String value = getProperties().get(key);
+                if(value != null){
+                    return value;
+                }
+                throw new RuntimeException("getVersion error, version: " + version);
+            }
+            return version;
+        }
+
+        public void setVersion(String version) {
+            this.version = version;
+        }
+
+        public String getClassifier() {
+            return classifier;
+        }
+
+        public void setClassifier(String classifier) {
+            this.classifier = classifier;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public String getScope() {
+            return scope;
+        }
+
+        public void setScope(String scope) {
+            this.scope = scope;
+        }
+    }
+
+    private static String getDependencyTagValue(String inputLine, String TagString) {
+        if (inputLine == null || TagString == null) {
+            return null;
+        }
+        inputLine = inputLine.trim();
+        String tmp = null;
+        if (inputLine.startsWith("<" + TagString + ">")) {
             tmp = inputLine.substring(inputLine.indexOf("<" + TagString + ">") + ("<" + TagString + ">").length());
             tmp = tmp.substring(0, tmp.indexOf("</" + TagString + ">"));
             System.out.println(TagString + ": " + tmp);
         }
+        if (TagString.equals("releaseUrl")) {
+            if(inputLine.startsWith("<!-- " + Dependency.BASE_RELEASE_URL)) {
+                tmp = inputLine.substring(inputLine.indexOf("<!-- ") + ("<!-- ").length());
+                tmp = tmp.substring(0, tmp.indexOf(" -->"));
+                tmp = tmp.trim();
+                System.out.println(TagString + ": " + tmp);
+            }
+        }
         return tmp;
     }
 
-    private void findDependencies(String inputDependencyManagementFilePath, List<String> downloadedDependenciesUrl, StringBuilder outputDependencies) {
+    private static void findDependencies(String inputDependencyManagementFilePath, List<String> downloadedDependenciesUrl, StringBuilder outputDependencies) {
         try {
             List<String> inputLines = FileUtils.readLines(new File(inputDependencyManagementFilePath), StandardCharsets.UTF_8);
+            boolean isProperties = false;
             boolean isDependencyManagement = false;
-            String baseUrl = null;
-            String groupId = null;
-            String artifactId = null;
-            String version = null;
-            String classifier = null;
-            String type = null;
-            String scope = null;
+            Dependency dependency = new Dependency();
             for (String inputLine : inputLines) {
                 inputLine = inputLine.trim();
-                if(inputLine.startsWith("<dependencyManagement>")) {
-                    isDependencyManagement = true;
-                }
-                if(inputLine.startsWith("</dependencyManagement>")) {
-                    isDependencyManagement = false;
-                }
-                if(!isDependencyManagement) {
-                    continue;
-                }
-                if(baseUrl == null && inputLine.startsWith("<!-- https://mvnrepository.com/artifact/")) {
-                    inputLine = inputLine.substring(inputLine.indexOf("<!-- https://mvnrepository.com/artifact/") + "<!-- ".length());
-                    inputLine = inputLine.substring(0, inputLine.indexOf(" "));
-                    inputLine = inputLine.replace(".", "/");
-                    inputLine = inputLine.replace("https://mvnrepository/com/artifact/", "https://repo1.maven.org/maven2/");
-                    baseUrl = inputLine;
-                    System.out.println("baseUrl: " + baseUrl);
-                }
-                if(baseUrl != null) {
-                    groupId = getTagString(inputLine, "groupId", groupId);
-                    artifactId = getTagString(inputLine, "artifactId", artifactId);
-                    version = getTagString(inputLine, "version", version);
-                    classifier = getTagString(inputLine, "classifier", classifier);
-                    type = getTagString(inputLine, "type", type);
-                    scope = getTagString(inputLine, "scope", scope);
-                }
-                if(baseUrl != null && inputLine.isEmpty()) {
-                    String fileName = artifactId  + "-" + version + ( classifier == null ? "" : "-" + classifier) + "." + ( type == null ? "jar" : type);
-                    String dependencyUrl = baseUrl + "/" + version + "/" + fileName;
-                    System.out.println("dependencyUrl: " + dependencyUrl);
-                    downloadedDependenciesUrl.add(dependencyUrl);
-                    if(scope == null) {
-                        outputDependencies.append("          <dependency>" + "\r\n");
-                        outputDependencies.append("              <groupId>").append(groupId).append("</groupId>").append("\r\n");
-                        outputDependencies.append("              <artifactId>").append(artifactId).append("</artifactId>").append("\r\n");
-                        outputDependencies.append(classifier == null ? "" : "              <classifier>" + classifier + "</classifier>" + "\r\n");
-                        outputDependencies.append(type == null ? "" : "              <type>" + type + "</type>" + "\r\n");
-                        outputDependencies.append("          </dependency>" + "\r\n");
-                        outputDependencies.append("\r\n");
+                {
+                    if (inputLine.startsWith("<properties>")) {
+                        isProperties = true;
+                        continue;
                     }
-                    if(Objects.equals(scope, "import")) {
-                        FileUtils.copyInputStreamToFile(TrustAllCerts.setTrusted(new URL(dependencyUrl).openConnection()).getInputStream(), new File("target" + "/" +fileName));
-                        FileUtils.readLines(new File("target" + "/" +fileName), StandardCharsets.UTF_8);
+                    if (inputLine.startsWith("</properties>")) {
+                        isProperties = false;
+                        continue;
                     }
-                    baseUrl = null;
-                    groupId = null;
-                    artifactId = null;
-                    version = null;
-                    classifier = null;
-                    type = null;
-                    scope = null;
+                    if (inputLine.startsWith("<dependencyManagement>")) {
+                        isDependencyManagement = true;
+                        continue;
+                    }
+                    if (inputLine.startsWith("</dependencyManagement>")) {
+                        isDependencyManagement = false;
+                        continue;
+                    }
+                }
+                if(isProperties) {
+                    if(inputLine.startsWith("<!-- ")) {
+                        continue;
+                    }
+                    if(inputLine.startsWith("<")) {
+                        String key = inputLine.substring(inputLine.indexOf("<") + ("<").length());
+                        key = key.substring(0, key.indexOf(">"));
+                        String value = StringUtils.substringBetween(inputLine, "<" + key + ">", "</" + key + ">");
+                        System.out.println("property "+ key + ": " + value);
+                        dependency.getProperties().put(key, value);
+                        continue;
+                    }
+                }
+                if(isDependencyManagement) {
+                    {
+                        String releaseUrl = getDependencyTagValue(inputLine, "releaseUrl");
+                        String groupId = getDependencyTagValue(inputLine, "groupId");
+                        String artifactId = getDependencyTagValue(inputLine, "artifactId");
+                        String version = getDependencyTagValue(inputLine, "version");
+                        String classifier = getDependencyTagValue(inputLine, "classifier");
+                        String type = getDependencyTagValue(inputLine, "type");
+                        String scope = getDependencyTagValue(inputLine, "scope");
+                        if (releaseUrl != null) dependency.setReleaseUrl(releaseUrl);
+                        if (groupId != null) dependency.setGroupId(groupId);
+                        if (artifactId != null) dependency.setArtifactId(artifactId);
+                        if (version != null) dependency.setVersion(version);
+                        if (classifier != null) dependency.setClassifier(classifier);
+                        if (type != null) dependency.setType(type);
+                        if (scope != null) dependency.setScope(scope);
+                    }
+                    if (inputLine.startsWith("</dependency>")) {
+                        if (dependency.getScope() == null || dependency.getScope().equals("compile")) {
+                            outputDependencies.append(dependency);
+                            outputDependencies.append("\r\n");
+                        }
+                        if (dependency.getScope() != null && dependency.getScope().equals("import")) {
+                            String downloadUrl = dependency.getDownloadUrl();
+                            String fileName = dependency.getFileName();
+                            System.out.println("downloadUrl: " + downloadUrl);
+                            System.out.println("fileName: " + fileName);
+                            try {
+                                FileUtils.copyInputStreamToFile(TrustAllCerts.setTrusted(new URL(downloadUrl).openConnection()).getInputStream(), new File("target" + "/" + fileName));
+                                FileUtils.readLines(new File("target" + "/" + fileName), StandardCharsets.UTF_8);
+                                // recursion
+                                downloadedDependenciesUrl.add(downloadUrl);
+                                findDependencies("target" + "/" + fileName, downloadedDependenciesUrl, outputDependencies);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        dependency = new Dependency();
+                    }
                 }
             }
         } catch (Exception e) {
