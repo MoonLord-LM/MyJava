77c675383ea22947b8bd9b6fc2e5380049c14986
月之领主LM <178910432@qq.com>
2022-07-23 18:31:59 +0800

    Update

--- security/src/test/java/cn/moonlord/security/HashTest.java
+++ security/src/test/java/cn/moonlord/security/HashTest.java
@@ -17,127 +17,45 @@ public class HashTest {
 
     public static Logger logger = LoggerFactory.getLogger(HashTest.class);
 
-    public static class sha256 {
+    public static class Sha256Test {
         @Test
-        public void success_1() {
-            byte[] source = new byte[0];
-            byte[] result = Hash.sha256(source);
-            byte[] compare = DigestUtils.sha256(source);
-            Assert.assertEquals(32, result.length);
-            Assert.assertArrayEquals(compare, result);
-        }
+        public void sha256() {
+            Assert.assertThrows(IllegalArgumentException.class, () -> Hash.sha256(null));
+            Assert.assertThrows(IllegalArgumentException.class, () -> Hash.sha256Hex(null));
 
-        @Test
-        public void success_2() {
-            byte[] source = new byte[]{(byte) 0x00, (byte) 0x11, (byte) 0x22, (byte) 0x33};
-            byte[] result = Hash.sha256(source);
-            byte[] compare = DigestUtils.sha256(source);
-            Assert.assertEquals(32, result.length);
-            Assert.assertArrayEquals(compare, result);
-        }
+            byte[] source1 = new byte[0];
+            Assert.assertArrayEquals(DigestUtils.sha256(source1), Hash.sha256(source1));
+            Assert.assertEquals(DigestUtils.sha256Hex(source1), Hash.sha256Hex(source1));
+            Assert.assertEquals("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", Hash.sha256Hex(source1));
 
-        @Test(expected = IllegalArgumentException.class)
-        public void error_1() {
-            Hash.sha256(null);
+            byte[] source2 = "测试".getBytes(StandardCharsets.UTF_8);
+            Assert.assertArrayEquals(DigestUtils.sha256(source2), Hash.sha256(source2));
+            Assert.assertEquals(DigestUtils.sha256Hex(source2), Hash.sha256Hex(source2));
+            Assert.assertEquals("6aa8f49cc992dfd75a114269ed26de0ad6d4e7d7a70d9c8afb3d7a57a88a73ed", Hash.sha256Hex(source2));
+
+            byte[] source3 = Random.generateBytes(1024);
+            Assert.assertArrayEquals(DigestUtils.sha256(source3), Hash.sha256(source3));
         }
     }
 
-    public static class sha256Hex {
+    public static class Sha512Test {
         @Test
-        public void success_1() {
-            byte[] source = new byte[0];
-            String result = Hash.sha256Hex(source);
-            String compare = DigestUtils.sha256Hex(source);
-            Assert.assertEquals(64, result.length());
-            Assert.assertEquals("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", result);
-            Assert.assertEquals(compare, result);
-        }
+        public void sha512() {
+            Assert.assertThrows(IllegalArgumentException.class, () -> Hash.sha512(null));
+            Assert.assertThrows(IllegalArgumentException.class, () -> Hash.sha512Hex(null));
 
-        @Test
-        public void success_2() {
-            byte[] source = new byte[]{(byte) 0x00, (byte) 0x11, (byte) 0x22, (byte) 0x33};
-            String result = Hash.sha256Hex(source);
-            String compare = DigestUtils.sha256Hex(source);
-            Assert.assertEquals(64, result.length());
-            Assert.assertEquals("aafa373bf008a855815ecb37d8bd52f6a8157cb5833c58edde6d530dbcf3f25d", result);
-            Assert.assertEquals(compare, result);
-        }
+            byte[] source1 = new byte[0];
+            Assert.assertArrayEquals(DigestUtils.sha512(source1), Hash.sha512(source1));
+            Assert.assertEquals(DigestUtils.sha512Hex(source1), Hash.sha512Hex(source1));
+            Assert.assertEquals("cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e", Hash.sha512Hex(source1));
 
-        @Test
-        public void success_3() {
-            byte[] source = "测试".getBytes(StandardCharsets.UTF_8);
-            String result = Hash.sha256Hex(source);
-            String compare = DigestUtils.sha256Hex(source);
-            Assert.assertEquals(64, result.length());
-            Assert.assertEquals("6aa8f49cc992dfd75a114269ed26de0ad6d4e7d7a70d9c8afb3d7a57a88a73ed", result);
-            Assert.assertEquals(compare, result);
-        }
+            byte[] source2 = "测试".getBytes(StandardCharsets.UTF_8);
+            Assert.assertArrayEquals(DigestUtils.sha512(source2), Hash.sha512(source2));
+            Assert.assertEquals(DigestUtils.sha512Hex(source2), Hash.sha512Hex(source2));
+            Assert.assertEquals("98fb26ea83ce0f08918c967392a26ab298740aff3c18d032983b88bcee2e16d152ef372778259ebd529ed01701ff01ac4c95ed94e3a1ab9272ab98daf11f076c", Hash.sha512Hex(source2));
 
-        @Test(expected = IllegalArgumentException.class)
-        public void error_1() {
-            Hash.sha256Hex(null);
-        }
-    }
-
-    public static class sha512 {
-        @Test
-        public void success_1() {
-            byte[] source = new byte[0];
-            byte[] result = Hash.sha512(source);
-            byte[] compare = DigestUtils.sha512(source);
-            Assert.assertEquals(64, result.length);
-            Assert.assertArrayEquals(compare, result);
-        }
-
-        @Test
-        public void success_2() {
-            byte[] source = new byte[]{(byte) 0x00, (byte) 0x11, (byte) 0x22, (byte) 0x33};
-            byte[] result = Hash.sha512(source);
-            byte[] compare = DigestUtils.sha512(source);
-            Assert.assertEquals(64, result.length);
-            Assert.assertArrayEquals(compare, result);
-        }
-
-        @Test(expected = IllegalArgumentException.class)
-        public void error_1() {
-            Hash.sha512(null);
-        }
-    }
-
-    public static class sha512Hex {
-        @Test
-        public void success_1() {
-            byte[] source = new byte[0];
-            String result = Hash.sha512Hex(source);
-            String compare = DigestUtils.sha512Hex(source);
-            Assert.assertEquals(128, result.length());
-            Assert.assertEquals("cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e", result);
-            Assert.assertEquals(compare, result);
-        }
-
-        @Test
-        public void success_2() {
-            byte[] source = new byte[]{(byte) 0x00, (byte) 0x11, (byte) 0x22, (byte) 0x33};
-            String result = Hash.sha512Hex(source);
-            String compare = DigestUtils.sha512Hex(source);
-            Assert.assertEquals(128, result.length());
-            Assert.assertEquals("fa54a45b24841a39ca6a3bab95d29ffdba8561b93d94f06f734a7a09c2370b79b2dd3b4efaf01eb3899206594e41cb5844031410d8ceee5ca4aa79c5461b4536", result);
-            Assert.assertEquals(compare, result);
-        }
-
-        @Test
-        public void success_3() {
-            byte[] source = "测试".getBytes(StandardCharsets.UTF_8);
-            String result = Hash.sha512Hex(source);
-            String compare = DigestUtils.sha512Hex(source);
-            Assert.assertEquals(128, result.length());
-            Assert.assertEquals("98fb26ea83ce0f08918c967392a26ab298740aff3c18d032983b88bcee2e16d152ef372778259ebd529ed01701ff01ac4c95ed94e3a1ab9272ab98daf11f076c", result);
-            Assert.assertEquals(compare, result);
-        }
-
-        @Test(expected = IllegalArgumentException.class)
-        public void error_1() {
-            Hash.sha512Hex(null);
+            byte[] source3 = Random.generateBytes(1024);
+            Assert.assertArrayEquals(DigestUtils.sha512(source3), Hash.sha512(source3));
         }
     }
 
