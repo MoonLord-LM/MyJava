de61c67dcad87e7b73d8a5a76320e706e52d3c53
MoonLord-LM <me@moonlord.cn>
2022-07-27 03:51:17 +0800

    Update

--- security/src/main/java/cn/moonlord/security/Provider.java
+++ security/src/main/java/cn/moonlord/security/Provider.java
@@ -1,11 +1,8 @@
 package cn.moonlord.security;
 
-import jdk.nashorn.internal.objects.NativeJSON;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 import java.security.Security;
-import java.util.Enumeration;
-import java.util.Locale;
 
 public class Provider {
 
@@ -14,181 +11,31 @@ public class Provider {
     }
 
     public synchronized static void init() {
-        java.security.Provider[] providers = Security.getProviders();
-        for (java.security.Provider provider : providers) {
-            if (provider instanceof BouncyCastleProvider) {
-                return;
-            }
-        }
-        Security.addProvider(new BouncyCastleProvider());
+        addBouncyCastleProvider();
     }
 
-    public static String showAllProviders() {
-        StringBuilder result = new StringBuilder();
-        java.security.Provider[] providers = Security.getProviders();
-        for (int i = 0; i < providers.length; i++) {
-            java.security.Provider provider = providers[i];
-            result.append("Provider [ " + i + " ]  [ " + provider.getName() + " ] " + " [ version " + provider.getVersion() + " ]  [ size " + provider.size() + " ] " + provider.getInfo());
-            result.append(System.lineSeparator());
-        }
-        return result.toString();
+    public synchronized static boolean hasBouncyCastleProvider() {
+        return Security.getProvider(BouncyCastleProvider.PROVIDER_NAME) != null;
     }
 
-    public static String showAllElements() {
-        StringBuilder result = new StringBuilder();
-        java.security.Provider[] providers = Security.getProviders();
-        for (int i = 0; i < providers.length; i++) {
-            java.security.Provider provider = providers[i];
-            for (Enumeration<Object> e = provider.keys(); e.hasMoreElements(); ) {
-                String key = (String) e.nextElement();
-                result.append("Provider [ " + i + " ]  [ " + provider.getName() + " ] ");
-                result.append(key + "  -  " + provider.get(key));
-                result.append(System.lineSeparator());
+    public synchronized static void addBouncyCastleProvider() {
+        if (!hasBouncyCastleProvider()) {
+            try {
+                Security.addProvider(new BouncyCastleProvider());
+            } catch (Exception e) {
+                throw new IllegalArgumentException("Provider addBouncyCastleProvider error, error message: " + e.getMessage(), e);
             }
         }
-        return result.toString();
     }
 
-    public static String showMessageDigests() {
-        StringBuilder result = new StringBuilder();
-        java.security.Provider[] providers = Security.getProviders();
-        for (int i = 0; i < providers.length; i++) {
-            java.security.Provider provider = providers[i];
-            for (Enumeration<Object> e = provider.keys(); e.hasMoreElements(); ) {
-                String key = (String) e.nextElement();
-                if (key.toUpperCase(Locale.ROOT).startsWith("MessageDigest.".toUpperCase(Locale.ROOT))) {
-                    result.append("Provider [ " + i + " ]  [ " + provider.getName() + " ] ");
-                    result.append(key.substring("MessageDigest.".length()) + "  -  " + provider.get(key));
-                    result.append(System.lineSeparator());
-                }
+    public synchronized static void removeBouncyCastleProvider() {
+        if (hasBouncyCastleProvider()) {
+            try {
+                Security.removeProvider(BouncyCastleProvider.PROVIDER_NAME);
+            } catch (Exception e) {
+                throw new IllegalArgumentException("Provider removeBouncyCastleProvider error, error message: " + e.getMessage(), e);
             }
         }
-        return result.toString();
-    }
-
-    public static String showSignatures() {
-        StringBuilder result = new StringBuilder();
-        java.security.Provider[] providers = Security.getProviders();
-        for (int i = 0; i < providers.length; i++) {
-            java.security.Provider provider = providers[i];
-            for (Enumeration<Object> e = provider.keys(); e.hasMoreElements(); ) {
-                String key = (String) e.nextElement();
-                if (key.toUpperCase(Locale.ROOT).startsWith("Signature.".toUpperCase(Locale.ROOT))) {
-                    result.append("Provider [ " + i + " ]  [ " + provider.getName() + " ] ");
-                    result.append(key.substring("Signature.".length()) + "  -  " + provider.get(key));
-                    result.append(System.lineSeparator());
-                }
-            }
-        }
-        return result.toString();
-    }
-
-    public static String showSecretKeyFactorys() {
-        StringBuilder result = new StringBuilder();
-        java.security.Provider[] providers = Security.getProviders();
-        for (int i = 0; i < providers.length; i++) {
-            java.security.Provider provider = providers[i];
-            for (Enumeration<Object> e = provider.keys(); e.hasMoreElements(); ) {
-                String key = (String) e.nextElement();
-                if (key.toUpperCase(Locale.ROOT).startsWith("SecretKeyFactory.".toUpperCase(Locale.ROOT))) {
-                    result.append("Provider [ " + i + " ]  [ " + provider.getName() + " ] ");
-                    result.append(key.substring("SecretKeyFactory.".length()) + "  -  " + provider.get(key));
-                    result.append(System.lineSeparator());
-                }
-            }
-        }
-        return result.toString();
-    }
-
-    public static String showPBKDF2s() {
-        StringBuilder result = new StringBuilder();
-        java.security.Provider[] providers = Security.getProviders();
-        for (int i = 0; i < providers.length; i++) {
-            java.security.Provider provider = providers[i];
-            for (Enumeration<Object> e = provider.keys(); e.hasMoreElements(); ) {
-                String key = (String) e.nextElement();
-                if (key.toUpperCase(Locale.ROOT).startsWith("SecretKeyFactory.PBKDF2".toUpperCase(Locale.ROOT))) {
-                    result.append("Provider [ " + i + " ]  [ " + provider.getName() + " ] ");
-                    result.append(key.substring("SecretKeyFactory.PBKDF2".length()) + "  -  " + provider.get(key));
-                    result.append(System.lineSeparator());
-                }
-            }
-        }
-        return result.toString();
-    }
-
-    public static String showSecureRandoms() {
-        StringBuilder result = new StringBuilder();
-        java.security.Provider[] providers = Security.getProviders();
-        for (int i = 0; i < providers.length; i++) {
-            java.security.Provider provider = providers[i];
-            for (Enumeration<Object> e = provider.keys(); e.hasMoreElements(); ) {
-                String key = (String) e.nextElement();
-                if (key.toUpperCase(Locale.ROOT).contains("SecureRandom.".toUpperCase(Locale.ROOT))) {
-                    result.append("Provider [ " + i + " ]  [ " + provider.getName() + " ] ");
-                    result.append(key.substring("SecureRandom.".length()) + "  -  " + provider.get(key));
-                    result.append(System.lineSeparator());
-                }
-            }
-        }
-        return result.toString();
-    }
-
-    public static String showCiphers() {
-        StringBuilder result = new StringBuilder();
-        java.security.Provider[] providers = Security.getProviders();
-        for (int i = 0; i < providers.length; i++) {
-            java.security.Provider provider = providers[i];
-            for (Enumeration<Object> e = provider.keys(); e.hasMoreElements(); ) {
-                String key = (String) e.nextElement();
-                if (key.toUpperCase(Locale.ROOT).startsWith("Cipher.".toUpperCase(Locale.ROOT))) {
-                    result.append("Provider [ " + i + " ]  [ " + provider.getName() + " ] ");
-                    result.append(key.substring("Cipher.".length()) + "  -  " + provider.get(key));
-                    result.append(System.lineSeparator());
-                }
-            }
-        }
-        return result.toString();
-    }
-
-    public static String showAesCiphers() {
-        StringBuilder result = new StringBuilder();
-        java.security.Provider[] providers = Security.getProviders();
-        for (int i = 0; i < providers.length; i++) {
-            java.security.Provider provider = providers[i];
-            for (Enumeration<Object> e = provider.keys(); e.hasMoreElements(); ) {
-                String key = (String) e.nextElement();
-                if (key.toUpperCase(Locale.ROOT).startsWith("Cipher.".toUpperCase(Locale.ROOT))) {
-                    if (key.toUpperCase(Locale.ROOT).contains("AES".toUpperCase(Locale.ROOT))) {
-                        if (!key.toUpperCase(Locale.ROOT).contains("With".toUpperCase(Locale.ROOT))) {
-                            result.append("Provider [ " + i + " ]  [ " + provider.getName() + " ] ");
-                            result.append(key.substring("Cipher.".length()) + "  -  " + provider.get(key));
-                            result.append(System.lineSeparator());
-                        }
-                    }
-                }
-            }
-        }
-        return result.toString();
-    }
-
-    public static String showChaChaCiphers() {
-        StringBuilder result = new StringBuilder();
-        java.security.Provider[] providers = Security.getProviders();
-        for (int i = 0; i < providers.length; i++) {
-            java.security.Provider provider = providers[i];
-            for (Enumeration<Object> e = provider.keys(); e.hasMoreElements(); ) {
-                String key = (String) e.nextElement();
-                if (key.toUpperCase(Locale.ROOT).startsWith("Cipher.".toUpperCase(Locale.ROOT))) {
-                    if (key.toUpperCase(Locale.ROOT).contains("ChaCha".toUpperCase(Locale.ROOT))) {
-                        result.append("Provider [ " + i + " ]  [ " + provider.getName() + " ] ");
-                        result.append(key.substring("Cipher.".length()) + "  -  " + provider.get(key));
-                        result.append(System.lineSeparator());
-                    }
-                }
-            }
-        }
-        return result.toString();
     }
 
 }
--- security/src/test/java/cn/moonlord/security/ProviderTest.java
+++ security/src/test/java/cn/moonlord/security/ProviderTest.java
@@ -10,6 +10,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import javax.crypto.Cipher;
 import javax.crypto.NoSuchPaddingException;
 import java.security.NoSuchAlgorithmException;
+import java.security.Security;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Locale;
 
 @SpringBootTest
 @RunWith(Enclosed.class)
@@ -17,68 +22,34 @@ public class ProviderTest {
 
     public static Logger logger = LoggerFactory.getLogger(ProviderTest.class);
 
-    public static class showAllProviders {
+    public static class BouncyCastleProviderTest {
         @Test
-        public void success_1() {
-            logger.info("showAllProviders" + "\r\n" + Provider.showAllProviders() + "\r\n");
-        }
-    }
-
-    public static class showAllElements {
-        @Test
-        public void success_1() {
-            logger.info("showAllElements" + "\r\n" + Provider.showAllElements() + "\r\n");
-        }
-    }
-
-    public static class showMessageDigests {
-        @Test
-        public void success_1() {
-            logger.info("showMessageDigests" + "\r\n" + Provider.showMessageDigests() + "\r\n");
-        }
-    }
-
-    public static class showSignatures {
-        @Test
-        public void success_1() {
-            logger.info("showSignatures" + "\r\n" + Provider.showSignatures() + "\r\n");
-        }
-    }
-
-    public static class showSecretKeyFactorys {
-        @Test
-        public void success_1() {
-            logger.info("showSecretKeyFactorys" + "\r\n" + Provider.showSecretKeyFactorys() + "\r\n");
-        }
-    }
-
-    public static class showPBKDF2s {
-        @Test
-        public void success_1() {
-            logger.info("showPBKDF2s" + "\r\n" + Provider.showPBKDF2s() + "\r\n");
-        }
-    }
-
-    public static class showSecureRandoms {
-        @Test
-        public void success_1() {
-            logger.info("showSecureRandoms" + "\r\n" + Provider.showSecureRandoms() + "\r\n");
-        }
-    }
-
-    public static class showCiphers {
-        @Test
-        public void success_1() {
-            logger.info("showCiphers" + "\r\n" + Provider.showCiphers() + "\r\n");
+        public void show() {
+            Provider.addBouncyCastleProvider();
+            java.security.Provider[] providers = Security.getProviders();
+            logger.info("Provider:");
+            for (int i = 0; i < providers.length; i++) {
+                java.security.Provider provider = providers[i];
+                logger.info("Provider  [ " + i + " ]  [ " + provider.getName() + " ] " + " [ version: " + provider.getVersion() + " ]  [ size: " + provider.size() + " ] " + provider.getInfo());
+            }
+            List<String> types = Arrays.asList("MessageDigest", "Signature", "SecretKeyFactory", "SecureRandom", "Cipher");
+            for (String type : types) {
+                logger.info("");
+                logger.info(type + ":");
+                for (int i = 0; i < providers.length; i++) {
+                    java.security.Provider provider = providers[i];
+                    for (Enumeration<Object> e = provider.keys(); e.hasMoreElements(); ) {
+                        String key = (String) e.nextElement();
+                        if (key.toUpperCase(Locale.ROOT).startsWith((type + ".").toUpperCase(Locale.ROOT))) {
+                            logger.info(type + " [ " + i + " ]  [ " + provider.getName() + " ] " + key.substring((type + ".").length()) + "  ->  " + provider.get(key));
+                        }
+                    }
+                }
+            }
         }
     }
 
     public static class showAesCiphers {
-        @Test
-        public void success_1() {
-            logger.info("showAesCiphers" + "\r\n" + Provider.showAesCiphers() + "\r\n");
-        }
-
         @Test()
         public void success_2() throws NoSuchPaddingException, NoSuchAlgorithmException {
             Provider.init();
@@ -90,11 +61,6 @@ public class ProviderTest {
     }
 
     public static class showChaChaCiphers {
-        @Test
-        public void success_1() {
-            logger.info("showChaChaCiphers" + "\r\n" + Provider.showChaChaCiphers() + "\r\n");
-        }
-
         @Test()
         public void success_2() throws NoSuchPaddingException, NoSuchAlgorithmException {
             Provider.init();
