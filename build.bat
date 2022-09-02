a786e44fb3ee0f9be7402fc71fd07a3792b99d53
MoonLord-LM <me@moonlord.cn>
2022-09-03 03:40:37 +0800

    Update

---
+++ build.bat
@@ -0,0 +1,12 @@
+:begin
+
+call mvn -B -U -e install --file "super-parent/pom.xml"
+call mvn -B -U -e install --file "version-management-bouncy-castle/pom.xml"
+call mvn -B -U -e install --file "version-management-bouncy-castle/pom.xml"
+call mvn -B -U -e install --file "version-management-tomcat/pom.xml"
+call mvn -B -U -e install --file "parent/pom.xml"
+call mvn -B -U -e install --file "dependencies/pom.xml"
+call mvn -B -U -e install --file "pom.xml"
+
+pause
+goto :begin
