fd42388e4af61041101dd0805666086bac090069
MoonLord-LM <me@moonlord.cn>
2022-09-03 10:32:24 +0800

    Update

--- build.bat
+++ build.bat
@@ -5,7 +5,6 @@ call mvn -B -U -e install --file "version-management-bouncy-castle/pom.xml"
 call mvn -B -U -e install --file "version-management-groovy/pom.xml"
 call mvn -B -U -e install --file "version-management-tomcat/pom.xml"
 call mvn -B -U -e install --file "parent/pom.xml"
-call mvn -B -U -e install --file "dependencies/pom.xml"
 call mvn -B -U -e install --file "pom.xml"
 
 pause
