34cd6f1d3f5e48518677b414b7af0fc52d671c26
MoonLord-LM <me@moonlord.cn>
2022-09-03 22:11:14 +0800

    Update

--- build.bat
+++ build.bat
@@ -1,11 +1,11 @@
 :begin
 
-call mvn -B -U -e install --file "super-parent/pom.xml"
-call mvn -B -U -e install --file "version-management-bouncy-castle/pom.xml"
-call mvn -B -U -e install --file "version-management-groovy/pom.xml"
-call mvn -B -U -e install --file "version-management-tomcat/pom.xml"
-call mvn -B -U -e install --file "parent/pom.xml"
-call mvn -B -U -e install --file "pom.xml"
+call mvn -B -U -e install -DcreateChecksum=true --file "super-parent/pom.xml"
+call mvn -B -U -e install -DcreateChecksum=true --file "version-management-bouncy-castle/pom.xml"
+call mvn -B -U -e install -DcreateChecksum=true --file "version-management-groovy/pom.xml"
+call mvn -B -U -e install -DcreateChecksum=true --file "version-management-tomcat/pom.xml"
+call mvn -B -U -e install -DcreateChecksum=true --file "parent/pom.xml"
+call mvn -B -U -e install -DcreateChecksum=true --file "pom.xml"
 
 pause
 cls
