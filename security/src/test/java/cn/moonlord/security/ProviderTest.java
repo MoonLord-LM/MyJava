95679ca5994aaad0486543f5095db6e3ae7f761c
MoonLord-LM <me@moonlord.cn>
2022-09-22 04:36:40 +0800

    Update ProviderTest.java

--- security/src/test/java/cn/moonlord/security/ProviderTest.java
+++ security/src/test/java/cn/moonlord/security/ProviderTest.java
@@ -12,6 +12,8 @@ import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKeyFactory;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.security.*;
 import java.util.Arrays;
 import java.util.Enumeration;
@@ -53,23 +55,61 @@ public class ProviderTest {
         @Test
         public void test() throws NoSuchPaddingException, NoSuchAlgorithmException {
             Provider.removeBouncyCastleProvider();
+
+            // Random
             Assert.assertThrows(NoSuchAlgorithmException.class, () -> SecureRandom.getInstance(Random.ALGORITHM_NONCE_AND_IV));
             Assert.assertThrows(NoSuchAlgorithmException.class, () -> SecureRandom.getInstance(Random.ALGORITHM_DEFAULT));
-            Assert.assertThrows(NoSuchAlgorithmException.class, () -> Cipher.getInstance("AES/CBC/PKCS7Padding"));
-            // Assert.assertThrows(NoSuchAlgorithmException.class, () -> Cipher.getInstance("ChaCha20-Poly1305"));
-            Assert.assertThrows(NoSuchAlgorithmException.class, () -> Cipher.getInstance("ChaCha20-IETF-Poly1305"));
-            Assert.assertThrows(NoSuchAlgorithmException.class, () -> Cipher.getInstance("XChaCha20-IETF-Poly1305"));
+            // Hash
             MessageDigest.getInstance(Hash.SHA256);
             MessageDigest.getInstance(Hash.SHA512);
+            // Aes
             Cipher.getInstance(Aes.AES_CIPHER_INSTANCE);
             KeyGenerator.getInstance(Aes.AES_KEY_ALGORITHM);
+            Assert.assertThrows(NoSuchAlgorithmException.class, () -> Cipher.getInstance("AES/CBC/PKCS7Padding"));
+            // Rsa
+            Assert.assertThrows(NoSuchAlgorithmException.class, () -> Cipher.getInstance(Rsa.RSA_CIPHER_INSTANCE));
             KeyPairGenerator.getInstance(Rsa.RSA_KEY_ALGORITHM);
+            // Pbkdf2
             SecretKeyFactory.getInstance(Pbkdf2.PBKDF2_ALGORITHM);
 
             Provider.addBouncyCastleProvider();
+
+            // Random
             SecureRandom.getInstance(Random.ALGORITHM_NONCE_AND_IV);
             SecureRandom.getInstance(Random.ALGORITHM_DEFAULT);
+            // Hash
+            MessageDigest.getInstance(Hash.SHA256);
+            MessageDigest.getInstance(Hash.SHA512);
+            // Aes
+            KeyGenerator.getInstance(Aes.AES_KEY_ALGORITHM);
+            Cipher.getInstance(Aes.AES_CIPHER_INSTANCE);
+            Cipher.getInstance("AES/CBC/PKCS7Padding");
+            // Rsa
+            KeyPairGenerator.getInstance(Rsa.RSA_KEY_ALGORITHM);
             Cipher.getInstance(Rsa.RSA_CIPHER_INSTANCE);
+            // Pbkdf2
+            SecretKeyFactory.getInstance(Pbkdf2.PBKDF2_ALGORITHM);
+
+            // ChaCha20Poly1305
+            Cipher.getInstance("ChaCha20-Poly1305");
+            Assert.assertThrows(NoSuchAlgorithmException.class, () -> Cipher.getInstance("ChaCha20-IETF-Poly1305"));
+            Assert.assertThrows(NoSuchAlgorithmException.class, () -> Cipher.getInstance("XChaCha20-IETF-Poly1305"));
+        }
+
+        @Test
+        public void exception() {
+            try {
+                Field field = Provider.PROVIDER_BOUNCY_CASTLE.getClass().getDeclaredField("PROVIDER_NAME");
+                Field modifiersField = Field.class.getDeclaredField("modifiers");
+                modifiersField.setAccessible(true);
+                modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+                field.setAccessible(true);
+                field.set(Provider.PROVIDER_BOUNCY_CASTLE.getClass(), "UNKNOWN");
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            Provider.addBouncyCastleProvider(); //TODO
+            Provider.removeBouncyCastleProvider(); //TODO
         }
     }
 
