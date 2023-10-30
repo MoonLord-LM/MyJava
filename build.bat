7f44cca4384cc405c44a11b482209f16030837da
MoonLord-LM <moonlord@msn.cn>
2023-10-31 02:05:57 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -5,19 +5,22 @@ call mvn -B -U -e install --file "0-maven-management/super-parent/pom.xml" ^
  -DcreateChecksum=true ^
  -Dmaven.wagon.http.ssl.insecure=true ^
  -Dmaven.wagon.http.ssl.allowall=true ^
- -Dmaven.wagon.http.ssl.ignore.validity.dates=true
+ -Dmaven.wagon.http.ssl.ignore.validity.dates=true ^
+ -Dmaven.test.skip=true
 
 call mvn -B -U -e install --file "0-maven-management/pom.xml" ^
  -DcreateChecksum=true ^
  -Dmaven.wagon.http.ssl.insecure=true ^
  -Dmaven.wagon.http.ssl.allowall=true ^
- -Dmaven.wagon.http.ssl.ignore.validity.dates=true
+ -Dmaven.wagon.http.ssl.ignore.validity.dates=true ^
+ -Dmaven.test.skip=true
 
  call mvn -B -U -e install --file "pom.xml" ^
  -DcreateChecksum=true ^
  -Dmaven.wagon.http.ssl.insecure=true ^
  -Dmaven.wagon.http.ssl.allowall=true ^
- -Dmaven.wagon.http.ssl.ignore.validity.dates=true
+ -Dmaven.wagon.http.ssl.ignore.validity.dates=true ^
+ -Dmaven.test.skip=true
 
 
 pause
