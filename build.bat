e0e6ca8f5d40f09c2f632690ed8b9181476761d3
MoonLord-LM <moonlord@msn.cn>
2023-10-31 01:51:57 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,36 +1,11 @@
 :begin
 
 
-call mvn -B -U -e install --file "super-parent/pom.xml" ^
+call mvn -B -U -e install --file "0-maven-management/pom.xml" ^
  -DcreateChecksum=true ^
  -Dmaven.wagon.http.ssl.insecure=true ^
  -Dmaven.wagon.http.ssl.allowall=true ^
  -Dmaven.wagon.http.ssl.ignore.validity.dates=true
-
- call mvn -B -U -e install --file "version-management-bouncy-castle/pom.xml" ^
- -DcreateChecksum=true ^
- -Dmaven.wagon.http.ssl.insecure=true ^
- -Dmaven.wagon.http.ssl.allowall=true ^
- -Dmaven.wagon.http.ssl.ignore.validity.dates=true
-
- call mvn -B -U -e install --file "version-management-groovy/pom.xml" ^
- -DcreateChecksum=true ^
- -Dmaven.wagon.http.ssl.insecure=true ^
- -Dmaven.wagon.http.ssl.allowall=true ^
- -Dmaven.wagon.http.ssl.ignore.validity.dates=true
-
- call mvn -B -U -e install --file "version-management-tomcat/pom.xml" ^
- -DcreateChecksum=true ^
- -Dmaven.wagon.http.ssl.insecure=true ^
- -Dmaven.wagon.http.ssl.allowall=true ^
- -Dmaven.wagon.http.ssl.ignore.validity.dates=true
-
- call mvn -B -U -e install --file "parent/pom.xml" ^
- -DcreateChecksum=true ^
- -Dmaven.wagon.http.ssl.insecure=true ^
- -Dmaven.wagon.http.ssl.allowall=true ^
- -Dmaven.wagon.http.ssl.ignore.validity.dates=true
-
 
  call mvn -B -U -e install --file "pom.xml" ^
  -DcreateChecksum=true ^
