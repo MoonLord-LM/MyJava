4913dbd198f3e57791bd05e7b1bb234cb9eae19b
MoonLord-LM <moonlord@msn.cn>
2023-10-31 02:04:34 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,12 @@
 :begin
 
 
+call mvn -B -U -e install --file "0-maven-management/super-parent/pom.xml" ^
+ -DcreateChecksum=true ^
+ -Dmaven.wagon.http.ssl.insecure=true ^
+ -Dmaven.wagon.http.ssl.allowall=true ^
+ -Dmaven.wagon.http.ssl.ignore.validity.dates=true
+
 call mvn -B -U -e install --file "0-maven-management/pom.xml" ^
  -DcreateChecksum=true ^
  -Dmaven.wagon.http.ssl.insecure=true ^
